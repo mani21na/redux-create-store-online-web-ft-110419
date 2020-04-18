c618e8a18ecc2d4f52338814d382903d9242bd88
Hanna Lee <mani21na@gmail.com>
2020-04-18 07:17:46 +0000

    Automatically backed up by Learn

--- js/createStore.js
+++ js/createStore.js
@@ -37,5 +37,5 @@ store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+    ㄴ색ㄷ.dispatch({ type: 'INCREASE_COUNT' });
 })
